62eb0bc8eea6d1d0e48469895dbf902a7409b423
BioCrossCoder <viscaria_dignics@163.com>
2025-06-17 20:02:51 +0800

    feat: implement basic config tab page of function details

--- web/src/components/FunctionDetail/index.tsx
+++ web/src/components/FunctionDetail/index.tsx
@@ -1,15 +1,117 @@
 'use client';
 
+import { Module } from '@/common/enum';
+import { deleteFunction, getFunctionDetails } from '@/server/logics/function';
+import { useQuery } from '@tanstack/react-query';
+import { Button, Descriptions, notification, Skeleton, Space, Tag } from 'antd';
+import EmptyPlaceHolder from '../common/EmptyPlaceHolder';
+import MarkdownPreview from '@uiw/react-markdown-preview';
+import { codeBlockInMarkdown, routePathOfOverviewPage } from '@/common/utils';
+import '@ant-design/v5-patch-for-react-19';
+import DeleteButton from '../common/DeleteButton';
+import { StatusCodes } from 'http-status-codes';
+import { redirect, RedirectType } from 'next/navigation';
+
 type Props = {
   name: string;
   namespace: string;
 };
 const FunctionDetail = (props: Props) => {
-  return (
-    <div>
-      <div>Name:{props.name}</div>
-      <div>NameSpace:{props.namespace}</div>
-    </div>
+  const { data, isPending, isError } = useQuery({
+    queryKey: [Module.Function, props.namespace, props.name],
+    queryFn: () => getFunctionDetails(props.namespace, props.name)
+  });
+  async function handleDelete() {
+    const resp = await deleteFunction(props.name, props.namespace);
+    if (resp.code === StatusCodes.NO_CONTENT) {
+      notification.success({
+        message: 'Delete Success!',
+        placement: 'top'
+      });
+    } else {
+      notification.error({
+        message: 'Delete failed!',
+        description: (
+          <MarkdownPreview
+            source={codeBlockInMarkdown('json', JSON.stringify(resp.data, null, 2))}
+          />
+        ),
+        placement: 'top'
+      });
+    }
+    redirect(routePathOfOverviewPage(Module.Function), RedirectType.replace);
+  }
+  return isError || !data ? (
+    <EmptyPlaceHolder />
+  ) : (
+    <Space direction="vertical" align="end">
+      <Descriptions
+        bordered
+        items={[
+          {
+            key: 'name',
+            label: 'Name',
+            children: isPending ? <Skeleton.Input /> : data.spec.displayName || data.metadata.name
+          },
+          {
+            key: 'description',
+            label: 'Description',
+            children: isPending ? <Skeleton.Input /> : data.spec.description
+          },
+          {
+            key: 'package',
+            label: 'Package',
+            children: isPending ? <Skeleton.Input /> : data.spec.package
+          },
+          {
+            key: 'module',
+            label: 'Module',
+            children: isPending ? <Skeleton.Input /> : data.spec.module
+          },
+          {
+            key: 'sources',
+            label: 'Sources',
+            children: isPending ? (
+              <Skeleton.Input />
+            ) : (
+              data.spec.sources.map(item => {
+                const {
+                  pulsar: { topic }
+                } = item;
+                return (
+                  <Tag key={topic} color="blue">
+                    {topic}
+                  </Tag>
+                );
+              })
+            )
+          },
+          {
+            key: 'sink',
+            label: 'Sink',
+            children: isPending ? (
+              <Skeleton.Input />
+            ) : (
+              <Tag color="blue">{data.spec.sink.pulsar.topic}</Tag>
+            )
+          },
+          {
+            key: 'configs',
+            label: 'Configs',
+            children: isPending ? (
+              <Skeleton.Input />
+            ) : (
+              <MarkdownPreview
+                source={codeBlockInMarkdown('json', JSON.stringify(data.spec.config, null, 2))}
+              />
+            )
+          }
+        ]}
+      />
+      <DeleteButton type={Module.Function} action={handleDelete}>
+        <Button type="primary">Delete</Button>
+      </DeleteButton>
+    </Space>
   );
 };
 
