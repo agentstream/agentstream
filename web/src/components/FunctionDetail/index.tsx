6134462ef2afdd8fc5c04d7bca855823f7cd4cf1
BioCrossCoder <viscaria_dignics@163.com>
2025-06-17 20:40:01 +0800

    feat: add tab options on function details page

--- web/src/components/FunctionDetail/index.tsx
+++ web/src/components/FunctionDetail/index.tsx
@@ -3,7 +3,7 @@
 import { Module } from '@/common/enum';
 import { getFunctionDetails } from '@/server/logics/function';
 import { useQuery } from '@tanstack/react-query';
-import { Button, Descriptions, Skeleton, Space, Tag } from 'antd';
+import { Button, Descriptions, Skeleton, Space, Tabs, Tag } from 'antd';
 import EmptyPlaceHolder from '../common/EmptyPlaceHolder';
 import MarkdownPreview from '@uiw/react-markdown-preview';
 import { codeBlockInMarkdown, routePathOfOverviewPage } from '@/common/utils';
@@ -16,6 +16,7 @@ type Props = {
   name: string;
   namespace: string;
 };
+
 const FunctionDetail = (props: Props) => {
   const { data, isPending, isError } = useQuery({
     queryKey: [Module.Function, props.namespace, props.name],
@@ -28,74 +29,107 @@ const FunctionDetail = (props: Props) => {
   return isError || !data ? (
     <EmptyPlaceHolder />
   ) : (
-    <Space direction="vertical" align="end">
-      <Descriptions
-        bordered
-        items={[
-          {
-            key: 'name',
-            label: 'Name',
-            children: isPending ? <Skeleton.Input /> : data.spec.displayName || data.metadata.name
-          },
-          {
-            key: 'description',
-            label: 'Description',
-            children: isPending ? <Skeleton.Input /> : data.spec.description
-          },
-          {
-            key: 'package',
-            label: 'Package',
-            children: isPending ? <Skeleton.Input /> : data.spec.package
-          },
-          {
-            key: 'module',
-            label: 'Module',
-            children: isPending ? <Skeleton.Input /> : data.spec.module
-          },
-          {
-            key: 'sources',
-            label: 'Sources',
-            children: isPending ? (
-              <Skeleton.Input />
-            ) : (
-              data.spec.sources.map(item => {
-                const {
-                  pulsar: { topic }
-                } = item;
-                return (
-                  <Tag key={topic} color="blue">
-                    {topic}
-                  </Tag>
-                );
-              })
-            )
-          },
-          {
-            key: 'sink',
-            label: 'Sink',
-            children: isPending ? (
-              <Skeleton.Input />
-            ) : (
-              <Tag color="blue">{data.spec.sink.pulsar.topic}</Tag>
-            )
-          },
-          {
-            key: 'configs',
-            label: 'Configs',
-            children: isPending ? (
-              <Skeleton.Input />
-            ) : (
-              <MarkdownPreview
-                source={codeBlockInMarkdown('json', JSON.stringify(data.spec.config, null, 2))}
+    <Tabs
+      defaultActiveKey="config"
+      items={[
+        {
+          key: 'config',
+          label: 'Configuration',
+          children: (
+            <Space direction="vertical" align="end">
+              <Descriptions
+                bordered
+                items={[
+                  {
+                    key: 'name',
+                    label: 'Name',
+                    children: isPending ? (
+                      <Skeleton.Input />
+                    ) : (
+                      data.spec.displayName || data.metadata.name
+                    )
+                  },
+                  {
+                    key: 'description',
+                    label: 'Description',
+                    children: isPending ? <Skeleton.Input /> : data.spec.description
+                  },
+                  {
+                    key: 'package',
+                    label: 'Package',
+                    children: isPending ? <Skeleton.Input /> : data.spec.package
+                  },
+                  {
+                    key: 'module',
+                    label: 'Module',
+                    children: isPending ? <Skeleton.Input /> : data.spec.module
+                  },
+                  {
+                    key: 'sources',
+                    label: 'Sources',
+                    children: isPending ? (
+                      <Skeleton.Input />
+                    ) : (
+                      data.spec.sources.map(item => {
+                        const {
+                          pulsar: { topic }
+                        } = item;
+                        return (
+                          <Tag key={topic} color="blue">
+                            {topic}
+                          </Tag>
+                        );
+                      })
+                    )
+                  },
+                  {
+                    key: 'sink',
+                    label: 'Sink',
+                    children: isPending ? (
+                      <Skeleton.Input />
+                    ) : (
+                      <Tag color="blue">{data.spec.sink.pulsar.topic}</Tag>
+                    )
+                  },
+                  {
+                    key: 'configs',
+                    label: 'Configs',
+                    children: isPending ? (
+                      <Skeleton.Input />
+                    ) : (
+                      <MarkdownPreview
+                        source={codeBlockInMarkdown(
+                          'json',
+                          JSON.stringify(data.spec.config, null, 2)
+                        )}
+                      />
+                    )
+                  }
+                ]}
               />
-            )
-          }
-        ]}
-      />
-      <DeleteButton type={Module.Function} action={handleDelete}>
-        <Button type="primary">Delete</Button>
-      </DeleteButton>
-    </Space>
+              <DeleteButton type={Module.Function} action={handleDelete}>
+                <Button type="primary">Delete</Button>
+              </DeleteButton>
+            </Space>
+          )
+        },
+        {
+          key: 'metrics',
+          label: 'Metrics',
+          children: 'Prometheus'
+        },
+        {
+          key: 'messages',
+          label: 'Messages',
+          children: 'Pulsar Admin API'
+        },
+        {
+          key: 'logs',
+          label: 'Logs',
+          children: 'K8S SDK Pod logs'
+        }
+      ]}
+    />
   );
 };
 
