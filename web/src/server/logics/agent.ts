2a8d22a89ccc150c01081b4d0693153aa610d4fd
BioCrossCoder <viscaria_dignics@163.com>
2025-07-09 23:16:07 +0800

    feat: enable edit agent basic configurations on page

--- web/src/server/logics/agent.ts
+++ web/src/server/logics/agent.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { AgentSpec, AgentStreamApiResp, KubernetesApiResp, ResourceData, ResourceList } from '@/common/types';
+import { AgentSpec, AgentStreamApiResp, FunctionSpec, KubernetesApiResp, ResourceData, ResourceList } from '@/common/types';
 import { client } from '../infra/k8s';
 import { ResourceKind } from '../common/enum';
 import { StatusCodes } from 'http-status-codes';
@@ -111,6 +111,73 @@ export async function deleteAgent(name: string, namespace: string): Promise<Agen
         };
     } catch (err) {
         const { code, body } = err as KubernetesApiResp;
+        return {
+            code,
+            data: deserializeJSON(body)
+        }
+    }
+}
+
+export async function updateAgent(form: Record<string, string> & { functions: string[] }): Promise<AgentStreamApiResp> {
+    const { name, namespace, description, model, googleApiKey, instruction, functions, sources, sink } = form;
+    const tools = (functions ?? []).map(f => {
+        const [namespace, name] = f.split('/')
+        return { namespace, name }
+    });
+    const {
+        metadata: { resourceVersion },
+    } = await getAgentDetails(namespace, name)
+    const body = {
+        apiVersion: `${group}/${version}`,
+        kind: ResourceKind.Agent,
+        metadata: {
+            name,
+            namespace,
+            resourceVersion
+        },
+        spec: {
+            description: description ?? '',
+            displayName: name,
+            instruction,
+            tools,
+            model: {
+                googleApiKey,
+                model
+            },
+            sources: (sources ?? '')
+                .split(',')
+                .filter(item => item !== '')
+                .map(topic => ({
+                    pulsar: {
+                        topic,
+                        subscriptionName: ''
+                    }
+                })),
+            sink: {
+                pulsar: {
+                    topic: sink ?? ''
+                }
+            },
+        }
+    }
+    try {
+        const resp = await client.replaceNamespacedCustomObject({
+            name,
+            group,
+            version,
+            namespace,
+            plural,
+            body
+        })
+        return {
+            code: StatusCodes.OK,
+            data: {
+                bid: `${namespace}/${name}`,
+                uid: (resp as ResourceData<FunctionSpec>).metadata.uid
+            }
+        }
+    } catch (err) {
+        const { code, body } = err as KubernetesApiResp
         return {
             code,
             data: deserializeJSON(body)
